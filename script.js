fcdf73204e47d0b5a78920210b6c1f11fbb835c2
PeterWSamy <86810702+PeterWSamy@users.noreply.github.com>
2024-06-02 19:08:05 +0300

    Added error checks and handle edge cases

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ document.getElementById('searchForm').addEventListener('submit', async function(
     event.preventDefault(); // Prevent the form from submitting in the traditional way
 
     const searchTerm = document.getElementById('searchInput').value;
-    const apiUrl = `https://api.example.com/search?q=${encodeURIComponent(searchTerm)}`;
+
+    if(searchTerm == ""){
+        alert("Please enter a topic")
+    }
+    const apiUrl = `https://topics-and-questions.vercel.app/search?q=${encodeURIComponent(searchTerm)}`;
 
     try {
         const response = await fetch(apiUrl);
@@ -13,15 +17,24 @@ document.getElementById('searchForm').addEventListener('submit', async function(
         const data = await response.json();
         displayResults(data);
     } catch (error) {
+        alert('Error fetching data')
         console.error('Error fetching data:', error);
     }
 });
 
 function displayResults(numbers) {
+    console.log(numbers['questionNumbers'])
     const tableBody = document.getElementById('resultsTable').querySelector('tbody');
     tableBody.innerHTML = '';
 
-    numbers.forEach(number => {
+    if(numbers['questionNumbers'].length == 0){
+        const row = document.createElement('tr');
+        const cell = document.createElement('td');
+        cell.textContent = "No children";
+        row.appendChild(cell);
+        tableBody.appendChild(row);
+    }
+    numbers['questionNumbers'].forEach(number => {
         const row = document.createElement('tr');
         const cell = document.createElement('td');
         cell.textContent = number;
